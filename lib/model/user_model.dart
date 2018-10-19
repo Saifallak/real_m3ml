043e2ad48a900d9806eb777cf3b7c1048a4fd910
Saifallak <Saifallak@yahoo.com>
2018-10-19 23:38:09 +0200

    usermodel

---
+++ lib/model/user_model.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class UserModel {
+  String key;
+  String name;
+  String email;
+  String phone;
+
+  UserModel.fromSnapShot(DataSnapshot snapshot)
+      : key = snapshot.key,
+        name = snapshot.value["Name"],
+        email = snapshot.value["Email"],
+        phone = snapshot.value["Phone"];
+}
